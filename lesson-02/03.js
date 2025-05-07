07ec2b73184f69609abd5693321e663fef93e656
s3wc <abdalovs715@gmail.com>
2025-05-07 20:33:03 +0500

    l2 final

--- lesson-02/03.js
+++ lesson-02/03.js
@@ -4,22 +4,17 @@
  * Результат сохраните в переменной grade.
  */
 
-const score = 70 // тестовое значение, можно изменять
-let grade
-switch (score) {
-    case score <= 49:
-        grade = 'F'
-        break;
-    case score <= 69:
-        grade = 'D'
-        break;
-    case score <= 79:
-        grade = 'C'
-        break;
-    case score <= 89:
-        grade = 'B'
-        break;
-    case score <= 100:
-        grade = 'A'
-        break;
+const score = 70; // тестовое значение, можно изменять
+let grade;
+
+if (score <= 49) {
+    grade = 'F';
+} else if (score <= 69) {
+    grade = 'D';
+} else if (score <= 79) {
+    grade = 'C';
+} else if (score <= 89) {
+    grade = 'B';
+} else {
+    grade = 'A';
 }
